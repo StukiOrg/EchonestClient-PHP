932940ac43c6b82390e1fa0aa8953d4386e0a3a0
Tom Anderson <tom.h.anderson@gmail.com>
2013-02-21 14:07:06 -0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,34 @@
+Echonest API PHP Library
+=====================
+This is a library to abstract the Echonest API 
+
+Installation
+------------
+  1. edit `composer.json` file with following contents:
+
+     ```json
+     "require": {
+        "gracenote/gracenote": "dev-master"
+     }
+     ```
+  2. install composer via `curl -s http://getcomposer.org/installer | php` (on windows, download
+     http://getcomposer.org/installer and execute it with PHP)
+  3. run `php composer.phar install`
+
+Use
+---
+Configure the service
+```php
+use Echonest\Service\Echonest;
+
+Echonest::configure($apiKey);
+```
+
+Run a query
+```php
+$response = Echonest::query('artist', 'biographies', array(
+    'id' => 'ARH6W4X1187B99274F',
+    'results' => '1',
+    'start' => '0',
+    'license' => 'cc-by-sa'
+));
---
+++ src/Echonest/Client/Echonest.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Echonest PHP Library
+ *
+ * @version 1.0.0
+ */
+
+namespace Echonest\Service;
+
+use Zend\Http\Client;
+use Zend\Http\Headers;
+use Zend\Json\Json;
+use Zend\I18n\Validator\Alnum as Alnum;
+
+final class Echonest {
+
+    static private $apiKey;
+
+    static public function getApiKey()
+    {
+        return self::$apiKey;
+    }
+
+    static public function setApiKey($value)
+    {
+        self::$apiKey = $value;
+    }
+
+    static public function configure($apiKey)
+    {
+        self::setApiKey($apiKey);
+    }
+
+    static public function query($api, $command, $options = array())
+    {
+        // Validate configuration
+        if (!self::getApiKey())
+            throw new \Exception('Echonest has not been configured');
+
+        $http = new Client();
+        $http->setUri('http://developer.echonest.com/api/v4/' . $api . '/' . $command);
+        $http->setOptions(array('sslverifypeer' => false));
+        $http->setMethod('GET');
+
+        $options['api_key'] = self::getApiKey();
+        if (!$options['format']) $options['format'] = 'json';
+        $http->setGetParameters($options);
+
+        $response = $http->send();
+        return Json::decode($response->getBody());
+    }
+}
